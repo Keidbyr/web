5bf73bfeae08ff97720c740612779429b30fa2bd
keidbyr <1109lenya@gmail.com>
2025-06-01 16:09:10 +0300

    2

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from articles.models import Article
 from . import forms
+from django.http import HttpResponse
 
 
 def article_list(request):
@@ -19,10 +20,35 @@ def article_create(request):
     if request.method == 'POST':
         form = forms.CreateArticle(request.POST, request.FILES)
         if form.is_valid():
-            instance =form.save(commit=False)
+            instance = form.save(commit=False)
             instance.author = request.user
             instance.save()
             return redirect('homepage')
     else:
         form = forms.CreateArticle()
-    return render(request, 'article_create.html', {'form': form})
+    return render(request, 'article_form.html', {'form': form})
+
+
+@login_required(login_url='accounts:login')
+def article_update(request, slug):
+    article = Article.objects.get(slug=slug)
+    if request.user.id == article.author.id:
+        form = forms.CreateArticle(request.POST, request.FILES, instance=article)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+        else:
+            form = forms.CreateArticle(instance=article)
+        return render(request, 'article_form.html', {'form': form})
+
+
+@login_required(login_url='accounts:login')
+def article_delete(request, slug):
+    article = Article.objects.get(slug=slug)
+    if request.user.id == article.user.id:
+        if request.method == 'POST':
+            article.delete()
+            return redirect('homepage')
+        return render(request, 'article_delete.html', {'article': article})
+    return HttpResponse('401 Unathorized', status=401)
